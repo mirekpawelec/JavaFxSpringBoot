3205a0865e4cbd629e3bee97ecfe82ce74d148fe
Mirek Pawelec <mirek.pawelec@wp.pl>
2018-10-24 00:58:36 +0200

    JFXSB-0001/added basic configuration

--- JavaFxSpringBoot/src/main/java/pl/javafx/spring/Main.java
+++ JavaFxSpringBoot/src/main/java/pl/javafx/spring/Main.java
@@ -6,32 +6,43 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import pl.javafx.spring.config.StageManager;
+import pl.javafx.spring.model.enums.FxmlView;
 
 @SpringBootApplication
-public class Main extends Application{
+public class Main extends Application {
 
 	protected ConfigurableApplicationContext context;
-	
-	
+	protected StageManager stageManager;
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
-	
+
 	@Override
 	public void init() throws Exception {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
-		String[] args = getParameters().getRaw().stream().toArray(String[]::new);
-		context = builder.run(args);
+		context = getSpringBootApplicationContext();
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		stageManager = context.getBean(StageManager.class, primaryStage);
+		displayScene();
 	}
 
 	@Override
 	public void stop() throws Exception {
-
+		context.stop();
 	}
-	
+
+	protected void displayScene() {
+		stageManager.switchScene(FxmlView.MAIN);
+	}
+
+	private ConfigurableApplicationContext getSpringBootApplicationContext() {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
+		String[] args = getParameters().getRaw().stream().toArray(String[]::new);
+		return builder.run(args);
+	}
+
 }
